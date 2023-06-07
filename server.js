a1032f2007048a5abc2f3eb196431c29ab8eb43d
javedsaqib94 <javed.saqib94@gmail.com>
2023-06-08 00:27:58 +1000

    new updates added

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connection.once('open', () => {
 
 
 //bringing all the models
-require("./models/Chatrooms");
+require("./models/Chatroom");
 require("./models/User");
 require("./models/Message");
 
@@ -30,54 +30,58 @@ const server = app.listen(8000, ()=> {
     console.log("server  listening on port  8000");
 });
 
-
-const io = require('socket.io')(server);
+const io = require("socket.io")(server, {
+    allowEIO3: true,
+    cors: {
+      origin: true,
+      methods: ['GET', 'POST'],
+      credentials: true
+    }
+  });
 const jwt= require('jwt-then');
 
 const Message = mongoose.model("Message");
 const User = mongoose.model("User");
+
 io.use(async (socket,next) => {
     try {       
         const token = socket.handshake.query.token; 
-        
         const payload = await jwt.verify(token, process.env.SECRET);
         socket.userId = payload.id;
         next();
-    } catch (err) {
-        
-    }
+    } catch (err) {}
 });
-
-io.on('connection', (socket) =>{
-    console.log("connected" + socket.userId);
-    
-    socket.on("disconnec" , () =>{
-        console.log("Disconnected" + socket.userId);
+io.on("connection", (socket) => {
+    console.log("Connected: " + socket.userId);
+  
+    socket.on("disconnect", () => {
+      console.log("Disconnected: " + socket.userId);
     });
-
-
-    socker.on("joinRoom", ({chatroomId}) =>{
-        socket.join(chatroomId);
-        console.log("A user joined Chatroom:" + chatroomId);
+  
+    socket.on("joinRoom", ({ chatroomId }) => {
+      socket.join(chatroomId);
+      console.log("A user joined chatroom: " + chatroomId);
     });
-
-    socker.on("leaveRoom", ({chatroomId}) =>{
-        socket.leave(chatroomId);
-        console.log("A user left Chatroom:" + chatroomId);
+  
+    socket.on("leaveRoom", ({ chatroomId }) => {
+      socket.leave(chatroomId);
+      console.log("A user left chatroom: " + chatroomId);
     });
-
-    socker.on("chatroomMessage", async ({chatroomId, message}) =>{
-        if(message.trim().length > 0 ){
-            const user = await User.findOne({_id: socket,userId});  
-            const message = new Message({chatroom: chatroomId, user: socket.userId, message, })
-            
-            io.to(chatroomId).emit("newMessage", {
-                message,
-                name: user.name,
-                userId:socket.userId,
-            }); 
-
-        await message.save();
-        }
+  
+    socket.on("chatroomMessage", async ({ chatroomId, message }) => {
+      if (message.trim().length > 0) {
+        const user = await User.findOne({ _id: socket.userId });
+        const newMessage = new Message({
+          chatroom: chatroomId,
+          user: socket.userId,
+          message,
+        });
+        io.to(chatroomId).emit("newMessage", {
+          message,
+          name: user.name,
+          userId: socket.userId,
+        });
+        await newMessage.save();
+      }
     });
-});
+  });
